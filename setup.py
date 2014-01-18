a6023bf6f0686776a2905c379fb713f1d8e74fbe
FND <fnd@localhost.localdomain>
2014-01-18 19:34:55 +0100

    changed function name for Python 3 compatibility
    not sure whether this will suffice though - @cdent, care to release?

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-
 import os
+
 from setuptools import setup, find_packages
 
+
 VERSION = '0.9.6'
+
 
 setup(
         namespace_packages = ['tiddlywebplugins'],
         name = 'tiddlywebplugins.templates',
         version = VERSION,
         description = 'A TiddlyWeb plugin with tools and packaging for templates.',
-        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
         author = 'Chris Dent',
         url = 'http://pypi.python.org/pypi/tiddlywebplugins.templates',
         packages = find_packages(exclude=['test']),
